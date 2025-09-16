99cf53160851d0c6757e5b3b283fa56965f455b1
Harikrishna Gunnala <89335989+itsmehk8333@users.noreply.github.com>
2025-09-16 14:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.getElementById("submitBtn").addEventListener("click", () => {
+  const firstName = document.getElementById("firstName").value.trim();
+  const lastName = document.getElementById("lastName").value.trim();
+  const phone = document.getElementById("phone").value.trim();
+  const email = document.getElementById("email").value.trim();
+
+  const output = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`;
+
+  alert(output);
+});
